bfae58d8e741e30f46e7356eff8b895fedc97b16
Jhonny <jagomezv@ufpso.edu.co>
2024-03-01 09:30:29 -0500

    Actualizacion del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,70 @@
-@extends('layouts.app')
+@extends('layouts.applogin')
+
+@section('tittle','Login')
 
 @section('content')
-<div class="container">
+
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Iniciar sesion</p>
+
+      <form method="POST" action="{{route('login')}}">
+        @csrf
+        <div class="input-group mb-3">
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="usuario@example.com">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+          </div>
+
+          <div class="col-4">
+            <a href="{{route('register')}}" class="btn btn-primary btn-block">Registro</a>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+        <div class="row">
+            <div class="col-12">
+            <p class="mb-1">
+                @if (Route::has('password.request'))
+                <a href="{{route('password.request')}}">Olvide mi contraseña</a>
+                @endif
+            </p>
+            </div>
+        </div>
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+@endsection
+
+
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +132,5 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> -->
+
