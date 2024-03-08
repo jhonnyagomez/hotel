e2c0bb25999d145fea701bc5b247d1d28e292383
Jhonny <jagomezv@ufpso.edu.co>
2024-03-08 09:55:31 -0500

    trabajo en la base de datos

--- resources/views/layouts/partial/header.blade.php
+++ resources/views/layouts/partial/header.blade.php
@@ -10,13 +10,20 @@
      <ul class="navbar-nav ml-auto">
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="{{asset('backend/dist/img/icons8-logout-96.png')}}" class="img-circle elevation-2" alt="User Image">
+                 <a href="#" class="">{{Auth::user()->name}}
+                     <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                         <img src="{{asset('backend/dist/img/icons8-logout-96.png')}}" class="img-circle elevation-2" alt="User Image">
+                     </a>
+                     <form id="logout-form" action="{{route('logout')}}" method="$_POST" class="d-none">
+                         @csrf
+                     </form>
+                 </a>
              </div>
              <div class="info">
-                 <a href="#" class="">{{Auth::user()->name}}</a>
+
              </div>
-             <form id="logout-form" action="{{route('logout')}}" method="$_POST" class="d-none"></form>
          </div>
      </ul>
  </nav>
  <!-- /.navbar -->
+ 
