3c4e38fd1942fe3fe36ceb624eb5b6e2d19a54bd
Jhonny <jagomezv@ufpso.edu.co>
2024-04-04 09:25:01 -0500

    Modificacion footer

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer" style="background-color: #FBF9F1;">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2014 <a href="#">The Rest Place</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+        <b>Version</b> 0.0.1
     </div>
 </footer>
