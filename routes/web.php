cdae5c5ea92e17aff2c2d3f698f9a124cc6f8617
Jhonny <jagomezv@ufpso.edu.co>
2024-04-11 09:15:29 -0500

    Se esta trabajando en el web.php, y se crean los controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +18,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::group(['middleware'=>['auth']], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    //products
+    Route::resource('products', ProductController::class);
+});
+
+Route::get('/about', function () { 
+    return 'Acerca de nosotros'; 
+});
+
+Route::get('/user/{id}', function ($id) { 
+    return 'ID de usuario: ' . $id; 
+});
+
+Route::get('/contacto', function () { 
+    return 'Página de contacto'; 
+})->name('contacto');
+
+Route::get('/usuario/{id}', function ($id) {
+    return 'ID de usuario: ' . $id;
+})->where('id', '[0-9]{3}');
+
+Route::prefix('admin')->group(function () { 
+    Route::get('/', function () { 
+    return 'Panel de administración'; 
+    }); 
+    Route::get('/users', function () { 
+    return 'Lista de usuarios'; 
+    }); 
+    }); 
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
